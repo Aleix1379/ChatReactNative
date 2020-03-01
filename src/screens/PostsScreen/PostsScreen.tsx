73a5d20282ba7186c41ed1266298e8112542de6b
Aleix <aleixmp@outlook.com>
2020-03-01 20:53:39 +0200

    Tablet layout

--- src/screens/PostsScreen/PostsScreen.tsx
+++ src/screens/PostsScreen/PostsScreen.tsx
@@ -9,50 +9,82 @@ import {NavigationScreenProp, NavigationState} from 'react-navigation';
 import Loading from '../../components/Loading/Loading';
 import styles from './PostsScreen.sass';
 import CommentService from '../../services/Comments';
+import DeviceInfo from 'react-native-device-info';
+import Comments from "../../components/Comments/Comments";
 
 interface Props {
-  navigation: NavigationScreenProp<NavigationState>;
+    navigation: NavigationScreenProp<NavigationState>;
 }
 
 const App: React.FC<Props> = ({navigation}) => {
-  const [showLoading, setShowLoading] = useState(false);
+    const [showLoading, setShowLoading] = useState(false);
+    const [showTabletLayout, setShowTabletLayout] = useState(false);
 
-  const dispatch = useDispatch();
-  const rootDispatcher = new RootDispatcher(dispatch);
+    const dispatch = useDispatch();
+    const rootDispatcher = new RootDispatcher(dispatch);
 
-  const showCommentOfPost = async (postId: number, name: string) => {
-    setShowLoading(true);
-    try {
-      const responseJson = await CommentService.getCommentsByPostId(postId);
-      rootDispatcher.updateComments(responseJson);
+    const showCommentOfPost = async (postId: number, name: string) => {
+        setShowLoading(true);
+        try {
+            const responseJson = await CommentService.getCommentsByPostId(postId);
+            rootDispatcher.updateComments(responseJson);
 
-      setShowLoading(false);
-      navigation.navigate('Comments', {title: name});
-    } catch (error) {
-      console.error(error);
-      setShowLoading(false);
-    }
-  };
+            setShowLoading(false);
+            if (!showTabletLayout) {
+                navigation.navigate('Comments', {title: name});
+            }
+        } catch (error) {
+            console.error(error);
+            setShowLoading(false);
+        }
+    };
 
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View style={styles.body}>
-          {showLoading && <Loading message="Loading..." />}
+    DeviceInfo.isLandscape()
+        .then(isLandscape => {
+            setShowTabletLayout(DeviceInfo.isTablet && isLandscape);
+        })
+        .catch(err => console.error(err));
 
-          <View style={styles.messages}>
-            <View style={styles.posts}>
-              <Posts
-                navigation={navigation}
-                postPressHandler={showCommentOfPost}
-              />
-            </View>
-          </View>
-        </View>
-      </SafeAreaView>
-    </>
-  );
+    return (
+        <>
+            <StatusBar barStyle="dark-content"/>
+            <SafeAreaView>
+                <View style={styles.body}>
+                    {showLoading && <Loading message="Loading..."/>}
+
+                    {
+                        !showTabletLayout &&
+                        <View style={styles.messages}>
+                            <View style={styles.posts}>
+                                <Posts
+                                    navigation={navigation}
+                                    postPressHandler={showCommentOfPost}
+                                />
+                            </View>
+                        </View>
+                    }
+
+                    {
+                        showTabletLayout &&
+                        <View style={styles.messages}>
+                            <View style={styles.posts}>
+                                <Posts
+                                    navigation={navigation}
+                                    postPressHandler={showCommentOfPost}
+                                />
+                            </View>
+
+                            <View>
+                                <Comments navigation={navigation}/>
+                            </View>
+
+                        </View>
+                    }
+
+                </View>
+            </SafeAreaView>
+        </>
+    );
 };
 
 export default App;
