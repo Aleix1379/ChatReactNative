513f7141871807471595a4c67338ac491a171113
Aleix <aleixmp@outlook.com>
2020-03-01 17:05:10 +0200

    Fix loading overlay now use all the height of the screen

--- src/components/Loading/Loading.sass.d.ts
+++ src/components/Loading/Loading.sass.d.ts
@@ -1,2 +1,1 @@
-export const loading: string;
 export const message: string;
--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {ActivityIndicator, Text, View} from 'react-native';
+import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
 
 import theme from '../../styles/theme.style';
 import styles from './Loading.sass';
+import {WindowUtils} from '../../utils/WindowUtils';
 
 interface Props {
   message?: string;
@@ -10,11 +11,27 @@ interface Props {
 
 const Loading: React.FC<Props> = ({message}) => {
   return (
-    <View style={styles.loading}>
+    <View style={reactStyle.loading}>
       <ActivityIndicator size="large" color={theme.SECONDARY_COLOR} />
       {message && <Text style={styles.message}>{message}</Text>}
     </View>
   );
 };
+
+const reactStyle = StyleSheet.create({
+  loading: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: WindowUtils.getWindowHeight(),
+    zIndex: 99999999,
+    backgroundColor: 'rgba(66,66,66,0.8)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 10,
+  },
+});
 
 export default Loading;
